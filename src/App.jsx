61093b249ad6a1020e35b9c5afebb551006fa23d
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-25 23:39:03 +0500

    all

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Page from './components/Page'
+import Nav_bar from './components/Nav_bar'
+
+function App() {
+  return (
+   <div>
+      <Nav_bar/>    
+      <Page/>
+   </div>
+  )
+}
+export default App
